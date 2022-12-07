2bba667dc1d3ca17fe5afe5f1e53a4b79430e698
Jay Austin McBee <jmcbee1@gmail.com>
2022-12-07 11:12:18 -0800

    add day five

---
+++ src/solutions/five.rs
@@ -0,0 +1,34 @@
+use super::solve::Solution;
+use std::path::Path;
+
+pub struct DayFive {}
+
+impl Solution for DayFive {
+    type Ret = ();
+    type Converted = ();
+
+    fn solve() -> Self::Ret {
+        todo!()
+    }
+
+    fn convert(input: &str) -> Self::Converted {
+        todo!()
+    }
+}
+
+impl DayFive {
+    pub fn some_method() {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::DayFive;
+
+    #[test]
+    fn test() {
+       todo!()
+    }
+}
